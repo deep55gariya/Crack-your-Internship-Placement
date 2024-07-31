a21a3d941408bb8fd621b3325ff94310bf63dbe4
Deepak Gariya <97430393+deep55gariya@users.noreply.github.com>
2024-08-01 00:21:44 +0530

    main

---
+++ Day-3/Leetcode-11(Container with most water).cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int l = 0;
+        int r= height.size()-1;
+        int maxarea=0;
+        while(l<r){
+            int area = min(height[l],height[r])*(r-l);
+            maxarea = max(maxarea,area);
+
+            if(height[l]<height[r]){
+                l++;
+            }
+            else{
+                r--;
+            }
+        }
+        
+        return maxarea;
+    }
+};
